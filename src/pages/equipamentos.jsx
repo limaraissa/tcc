1ccf87cde83f3a4b6838dd72910b617579426fa9
tinaraissa88 <raissafabriciolima3@gmail.comm>
2024-09-17 18:48:52 -0300

    commit imagem rodando

--- src/pages/equipamentos.jsx
+++ src/pages/equipamentos.jsx
@@ -1,18 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Coracao from "../assets/batimentos.png";
 import Temp from "../assets/temperatura.png";
 import Mod from "../assets/modulo.png";
 import Dis from "../assets/display.png";
 import Boneco from "../assets/boneco.png";
 import Arduino from "../assets/arduino.png";
-
 import Header from '../componentes/heather';
 
 export default function App() {
+  // Estados para controlar a rotação de cada imagem
+  const [isRotated, setIsRotated] = useState({
+    coracao: false,
+    temp: false,
+    arduino: false,
+    mod: false,
+    dis: false,
+    boneco: false,
+  });
+
+  // Função para alternar a rotação de uma imagem específica
+  const handleRotate = (item) => {
+    setIsRotated((prevState) => ({
+      ...prevState,
+      [item]: !prevState[item],
+    }));
+  };
+
   return (
     <main className="bg-fundo bg-no-repeat min-[430px]:h-[2700px] lg:h-[800px] font-poppins text-poppins">
       <div className="z-50">
-        <Header/>
+        <Header />
       </div>
       
       <h1 className="text-white font-bold text-center pt-11 text-7xl">BAY<span className="text-vermelho font-bold lg:text-7xl">MAX!</span></h1>
@@ -25,17 +42,32 @@ export default function App() {
       <div className="flex flex-col items-center justify-center lg:flex-row lg:flex-wrap lg:justify-center lg:px-8 bg-fundo">
         
         <div className="w-[300px] h-[500px] mt-[40px] bg-white rounded-[10px] flex flex-col items-center justify-center lg:w-[325px] lg:mx-4 lg:mt-[20px]">
-          <img src={Coracao} alt="sensor" className=""/>
+          <img 
+            src={Coracao} 
+            alt="sensor" 
+            className={`transition-transform duration-1000 ease-in-out ${isRotated.coracao ? 'rotate-360' : ''}`}
+            onClick={() => handleRotate('coracao')}
+          />
           <h1 className="font-semibold text-[32px] w-[287px] text-center mt-4">Sensor de Pulso e Frequência Cardíaca</h1>
         </div>
 
         <div className="w-[300px] h-[500px] mt-[40px] bg-white rounded-[10px] flex flex-col items-center justify-center lg:w-[325px] lg:mx-4 lg:mt-[20px]">
-          <img src={Temp} alt="sensor" className=""/>
+          <img 
+            src={Temp} 
+            alt="sensor" 
+            className={`transition-transform duration-1000 ease-in-out ${isRotated.temp ? 'rotate-360' : ''}`}
+            onClick={() => handleRotate('temp')}
+          />
           <h1 className="font-semibold text-[32px] w-[287px] text-center mt-4">DS1820 temperatura</h1>
         </div>
 
         <div className="w-[300px] h-[500px] mt-[40px] bg-white rounded-[10px] flex flex-col items-center justify-center lg:w-[325px] lg:mx-4 lg:mt-[20px]">
-          <img src={Arduino} alt="placa" className=""/>
+          <img 
+            src={Arduino} 
+            alt="placa" 
+            className={`transition-transform duration-1000 ease-in-out ${isRotated.arduino ? 'rotate-360' : ''}`}
+            onClick={() => handleRotate('arduino')}
+          />
           <h1 className="font-semibold text-[32px] w-[287px] text-center mt-4">Arduino Uno</h1>
         </div>
 
@@ -43,22 +75,37 @@ export default function App() {
 
       <div className="flex flex-col items-center justify-center lg:flex-row lg:mb-8 lg:flex-wrap lg:justify-center lg:px-8 bg-fundo">
         
-        <div className="w-[300px] h-[500px] mt-[50px] bg-white  rounded-[10px] flex flex-col items-center justify-center lg:w-[325px] lg:mx-4 lg:mt-[20px]">
-          <img src={Mod} alt="modulo" className=""/>
+        <div className="w-[300px] h-[500px] mt-[50px] bg-white rounded-[10px] flex flex-col items-center justify-center lg:w-[325px] lg:mx-4 lg:mt-[20px]">
+          <img 
+            src={Mod} 
+            alt="modulo" 
+            className={`transition-transform duration-1000 ease-in-out ${isRotated.mod ? 'rotate-360' : ''}`}
+            onClick={() => handleRotate('mod')}
+          />
           <h1 className="font-semibold text-[32px] w-[287px] text-center mt-4">Módulo WiFi + Bluetooth ESP32</h1>
         </div>
 
-        <div className="w-[300px] h-[500px] mt-[50px] bg-white rounded-[10px]  flex flex-col items-center justify-center lg:w-[325px] lg:mx-4 lg:mt-[20px]">
-          <img src={Dis} alt="display" className=""/>
+        <div className="w-[300px] h-[500px] mt-[50px] bg-white rounded-[10px] flex flex-col items-center justify-center lg:w-[325px] lg:mx-4 lg:mt-[20px]">
+          <img 
+            src={Dis} 
+            alt="display" 
+            className={`transition-transform duration-1000 ease-in-out ${isRotated.dis ? 'rotate-360' : ''}`}
+            onClick={() => handleRotate('dis')}
+          />
           <h1 className="font-semibold text-[32px] w-[287px] text-center mt-4">Display LCD 16x2</h1>
         </div>
 
-        <div className="w-[300px] h-[500px] mt-[50px]  bg-white rounded-[10px]  flex flex-col items-center justify-center lg:w-[325px] lg:mx-4 lg:mt-[20px]">
-          <img src={Boneco} alt="boneco" className=""/>
+        <div className="w-[300px] h-[500px] mt-[50px] bg-white rounded-[10px] flex flex-col items-center justify-center lg:w-[325px] lg:mx-4 lg:mt-[20px]">
+          <img 
+            src={Boneco} 
+            alt="boneco" 
+            className={`transition-transform duration-1000 ease-in-out ${isRotated.boneco ? 'rotate-360' : ''}`}
+            onClick={() => handleRotate('boneco')}
+          />
           <h1 className="font-semibold text-[32px] w-[287px] text-center mt-4">Módulo RTC DS1302</h1>
         </div>
 
       </div>
     </main>
-  )
+  );
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,52 +1,59 @@
-import React from 'react'
+import React, { useRef } from 'react';
 
-import img2 from '../public/image 17.png'
-import baymax from '../public/baymaxfundo.png'
-import Header from '../componentes/heather'
+import img2 from '../public/image 17.png';
+import baymax from '../public/baymaxfundo.png';
+import Header from '../componentes/heather';
 
+export default function Home() {
+  // Cria uma referência para a div de destino
+  const targetDivRef = useRef(null);
 
-export default function home() {
+  // Função que rola para a div de destino
+  const scrollToTargetDiv = () => {
+    if (targetDivRef.current) {
+      targetDivRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
   return (
-  <main className=' z-0'>
-      
-     <div className='bg-fundo '>
-     <Header />
-      
-      <div className='bg-fundo z-0'>
-          
-          <h1 className='sm:flex text-5xl text-center text-white font-poppins font-bold block mt-4 lg:hidden '>BAY<span className='text-vermelho'>MAX!</span></h1>
-          <p className='sm:text-center text-white font-poppins m-10 text-xl text-wrap block lg:hidden '>Assim como no filme Big hero o personagem Baymax tem o objetivo de auxiliar na sua saúde. O robo vem com a intenção de tornar seu dia a dia mais prático e funcional venha conhecer mais.</p>
-          <div className='sm:flex sm:justify-center lg:flex lg:justify-end lg:space-x-80 '>
+    <main className='z-0'>
+      <div className='bg-fundo'>
+        <Header />
+        
+        <div className='bg-fundo z-0'>
+          <h1 className='sm:flex text-5xl text-center text-white font-poppins font-bold block mt-4 lg:hidden'>BAY<span className='text-vermelho'>MAX!</span></h1>
+          <p className='sm:text-center text-white font-poppins m-10 text-xl text-wrap block lg:hidden'>Assim como no filme Big hero o personagem Baymax tem o objetivo de auxiliar na sua saúde. O robo vem com a intenção de tornar seu dia a dia mais prático e funcional venha conhecer mais.</p>
+          <div className='sm:flex sm:justify-center lg:flex lg:justify-end lg:space-x-80'>
             <img src={baymax} alt="" className='sm:w-96 block lg:hidden' />
           </div>
+        </div>
+        
+        <h1 className='lg:text-white lg:font-poppins lg:font-bold lg:text-9xl lg:flex lg:mr-12 lg:ml-100 lg:relative lg:mt-36 hidden sm:block md:block'>BAY<span className='text-vermelho'>MAX!</span></h1>
+        <div className='flex gab-96'>
+          <img src={baymax} alt="" className='flex translate-y-4 relative z-10 mt-0 z-20 ml-20 hidden sm:block md:block' />
+          <p className='flex text-white font-poppins font-light text-2xl relative mr-20 ml-52 z-10 mt-4 mb-4 hidden sm:block md:block'>
+            Assim como no filme Big hero o personagem Baymax <br /> tem o objetivo de auxiliar na sua saúde. O robô vem <br /> com a intenção de tornar seu dia a dia mais prático e<br/> funcional venha conhecer mais.<br />
+            <button 
+              className='bg-vermelho flex lg:text-white lg:text-xl lg:justify-center lg:mr-50 bg-reed lg:h-24 lg:w-72 lg:text-white lg:font-poppins lg:font-bold lg:rounded-2xl lg:mt-20 hidden sm:block' 
+              onClick={scrollToTargetDiv} // Adiciona o evento de clique
+            >
+              BAYMAX
+            </button>
+          </p>
+        </div>
+
+        {/* A div de destino que queremos rolar até ela */}
+        <div ref={targetDivRef} className='flex items-center justify-center w-9/12 mx-auto bg-fundo space-x-44 mt-36 pb-28'>
+          <div className='text-white space-y-6 font-poppins font-medium text-lg hidden sm:block'>
+            <p>O primeiro artigo destaca como a tecnologia está<br/>  beneficiando a saúde, e explora o <br/> desenvolvimento de novos materiais e novas<br/> ferramentas, transformando e inovando a <br/> área dasaúde.</p>
+            <p>O segundo artigo vai falar sobre os avanços<br/> da tecnologia na área da saúde. O instituto<br/> IAM é uma comunidade dedicada a pesquisa<br/> do desenvolvimento de sistemas onde foram<br/> feitos testes que possibilitaram a aplicação<br/> de rôbos na medicina.</p>
+            <p>Já o terceiro artigo vai falar sobre como os<br/> rôbos reduzem drasticamente o tempo de<br/> execução das tarefas, impulsionando a<br/> eficiência operacional, não precisam de<br/> férias ou adoecem, garantindo operações<br/> continuas e consistentes e eles também<br/> mantêm precisão constante evitando falhas.</p>
+          </div>
+          <div className='ml-12 flex items-center justify-center'>
+            <img src={img2} alt="" className='hidden sm:block' />
+          </div>
+        </div>
       </div>
-      
-      
-      <h1 className='lg:text-white lg:font-poppins lg:font-bold lg:text-9xl lg:flex  lg:mr-12 lg:ml-100 lg:relative lg:mt-36 hidden sm:block md:block'>BAY<span className='text-vermelho'>MAX!</span></h1>
-        <div className='flex  gab-96  '>
-          <img src={baymax} alt=""  className='flex translate-y-4 relative z-10 mt-0 z-20 ml-20 hidden sm:block md:block'/>
-          <p className='flex text-white font-poppins font-light text-2xl relative mr-20 ml-52 z-10 mt-4 mb-4 hidden sm:block md:block'>Assim como no filme Big hero o personagem Baymax <br/> tem o objetivo de auxiliar na sua saúde. O robô vem <br/> com a intenção de tornar seu dia a dia mais prático e<br/> funcional venha conhecer mais.<br/><button className=' bg-vermelho flex lg:text-white lg:text-xl lg:justify-center lg:mr-50 bg-reed	lg:h-24 lg:w-72	lg:text-white lg:font-poppins lg:font-bold lg:rounded-2xl lg:mt-20 hidden sm:block 	'>BAYMAX</button></p>
-          
-      </div>
-
-
-
-
-      
-      <div className='flex items-center justify-center w-9/12 mx-auto bg-fundo space-x-44'>
-      <div className='mt-36 pb-28 text-white space-y-6 font-poppins font-medium text-lg hidden sm:block'>
-        <p>O primeiro artigo destaca como a tecnologia está<br/>  beneficiando a saúde, e explora o <br/> desenvolvimento de novos materiais e novas<br/> ferramentas, transformando e inovando a <br/> área dasaúde.</p>
-        <p>O segundo artigo vai falar sobre os avanços<br/> da tecnologia na área da saúde. O instituto<br/> IAM é uma comunidade dedicada a pesquisa<br/> do desenvolvimento de sistemas onde foram<br/> feitos testes que possibilitaram a aplicação<br/> de rôbos na medicina.</p>
-        <p>Já o terceiro artigo vai falar sobre como os<br/> rôbos reduzem drasticamente o tempo de<br/> execução das tarefas, impulsionando a<br/> eficiência operacional, não precisam de<br/> férias ou adoecem, garantindo operações<br/> continuas e consistentes e eles também<br/> mantêm precisão constante evitando falhas.</p>
-      </div>
-      <div className='ml-12 flex items-center justify-center'>
-        <img src={img2} alt="" className='hidden sm:block' />
-      </div>
-</div>
-
-
-    </div>
-  </main>
-   
-  )
+    </main>
+  );
 }
