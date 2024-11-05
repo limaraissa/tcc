fd3b25cf22eef9423e177c25d37fa8ec8fd5e777
tinaraissa88 <raissafabriciolima3@gmail.comm>
2024-11-04 22:33:28 -0300

    adicionando o vidio do jogo

--- src/pages/jogo.jsx
+++ src/pages/jogo.jsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import Header from '../componentes/heather'
-import Baymax from "../public/baymaxfundo.png"
-import Coracao from "../public/coracao.png"
-import Coracao2 from "../public/coracao2.png"
+
 
 
 export default function App() {
@@ -14,14 +12,14 @@ export default function App() {
                 <section>
                     {/* Nova seção "Guia de Utilização" */}
                 <div className=" flex flex-col items-center p-12 bg-azulfora outer-container m-12 border rounded-lg mt-24">
-                    <h2 className="text-white font-semibold text-center pt-4 text-4xl py-14">
-                        <span className="text-reed font-semibold text-4xl">Jo</span>go
+                    <h2 className="text-white font-semibold text-center pt-4 text-7xl py-14">
+                        <span className="text-reed font-semibold text-7xl">Jo</span>go
                     </h2>
 
                     <div class="mt-14 max-w-4xl w-full mx-auto">
                         <div class="aspect-video">
                             <iframe class="w-full h-full rounded-lg shadow-lg" 
-                                    src="https://www.youtube.com/embed/xV7ZzeBR3oc" 
+                                    src="https://www.youtube.com/embed/-iR0H5FqaAw" 
                                     title="YouTube video player" 
                                     frameborder="0" 
                                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
